0d9e11c466525301b69723db4532117c92b5b468
IDDna <romanov1371@gmail.com>
2022-09-10 20:27:47 +0300

    Implement basic Gameboard factory and write Gameboards tests

---
+++ src/factories/Gameboard.js
@@ -0,0 +1,42 @@
+import Ship from './Ship'
+
+class Gameboard {
+  constructor() {
+    this.ships = []
+    this.hittedCells = []
+  }
+
+  placeShip(length, coordinates) {
+    const ship = new Ship(length, coordinates)
+    this.ships.push(ship)
+  }
+
+  receiveAttack(coordinates) {
+    let hittedShip
+
+    const isHit = this.ships.some(ship => {
+      for (let cords of Object.keys(ship.coordinates)) {
+        if (cords == coordinates) {
+          hittedShip = ship
+          return true
+        }
+      }
+
+      return false
+    })
+
+    if (isHit) {
+      hittedShip.hit(coordinates)
+      if (hittedShip.isSunk()) hittedShip.sunk = true
+    }
+
+    this.hittedCells.push({ coordinates: isHit })
+    return isHit
+  }
+
+  isAllShipsSunk() {
+    return this.ships.every(ship => ship.sunk)
+  }
+}
+
+export default Gameboard
---
+++ src/factories/tests/Gameboard.test.js
@@ -0,0 +1,37 @@
+import Gameboard from '../gameboard'
+
+let gameboard
+
+beforeEach(() => {
+  gameboard = new Gameboard()
+  gameboard.placeShip(4, [2, 4])
+  gameboard.placeShip(2, [4, 7])
+})
+
+test('Returns true if any ship is hitted', () => {
+  expect(gameboard.receiveAttack([2, 4])).toBeTruthy()
+  expect(gameboard.receiveAttack([3, 4])).toBeTruthy()
+  expect(gameboard.receiveAttack([4, 4])).toBeTruthy()
+  expect(gameboard.receiveAttack([5, 4])).toBeTruthy()
+})
+
+test('Returns false if no ship is hitted', () => {
+  expect(gameboard.receiveAttack([1, 4])).toBeFalsy()
+  expect(gameboard.receiveAttack([5, 6])).toBeFalsy()
+})
+
+test('Returns true if all ships are sunk', () => {
+  gameboard.receiveAttack([2, 4])
+  gameboard.receiveAttack([3, 4])
+  gameboard.receiveAttack([4, 4])
+  gameboard.receiveAttack([5, 4])
+  gameboard.receiveAttack([4, 7])
+  gameboard.receiveAttack([5, 7])
+  expect(gameboard.isAllShipsSunk()).toBeTruthy()
+})
+
+test('Returns false if not all ships are sunk', () => {
+  gameboard.receiveAttack([4, 7])
+  gameboard.receiveAttack([5, 7])
+  expect(gameboard.isAllShipsSunk()).toBeFalsy()
+})
