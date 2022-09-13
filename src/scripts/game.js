5d6109826858d194fbf9b9f7f3849392c1f528df
IDDna <romanov1371@gmail.com>
2022-09-13 17:34:43 +0300

    Create basic css

---
+++ src/scripts/game.js
@@ -0,0 +1,7 @@
+import Player from '../factories/Player'
+import Gameboard from '../factories/Gameboard'
+
+const player = new Player()
+const computer = new Player(computer)
+const playerBoard = new Gameboard()
+const computerBoard = new Gameboard()
